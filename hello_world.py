ba8d8a731bb69ea26948855d4a64cee2f9074401
Jean28518 <jf_v@gmx.de>
2021-08-20 15:41:40 +0200

    Rearanged repo for smooth start

---
+++ hello_world.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+import jessentials
+import jfiles
+import jfolders
+
+def main():
+    print("Hello World!")
+    print("Whith this script you can simply start developing...")
+
+if __name__ == "__main__":
+    main()
